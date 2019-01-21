1353af5fc3966801bca68a69a388c1f46c89c650
minul islam <maanpatwary@gmail.com>
2019-01-21 07:09:17 +0600

    fixres Facade

--- src/Facade/Multicoin.php
+++ src/Facade/Multicoin.php
@@ -4,18 +4,10 @@ namespace Multicoin\Api\Facade;
 
 use Illuminate\Support\Facades\Facade;
 
-/**
- * @see \Multicoin\Api\Multicoin
- */
 class Multicoin extends Facade
 {
-    /**
-     * Get the registered name of the component.
-     *
-     * @return string
-     */
     protected static function getFacadeAccessor()
     {
-        return 'Multicoin';
+        return 'multicoin';
     }
 }
