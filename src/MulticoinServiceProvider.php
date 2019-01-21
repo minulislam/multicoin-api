18f8f19443e04b15fe6497068ba4ea40a33cab7e
minul islam <maanpatwary@gmail.com>
2019-01-21 07:11:13 +0600

    remove differ

--- src/MulticoinServiceProvider.php
+++ src/MulticoinServiceProvider.php
@@ -7,12 +7,6 @@ use Illuminate\Support\ServiceProvider;
 class MulticoinServiceProvider extends ServiceProvider
 {
     /**
-     * Indicates if loading of the provider is deferred.
-     *
-     * @var bool
-     */
-    protected $defer = true;
-    /**
      * Bootstrap the application services.
      */
     public function boot()
