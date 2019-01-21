da996822196673124b7d2cbe12847ac70a259f61
minul islam <maanpatwary@gmail.com>
2019-01-21 12:20:16 +0600

    updated git ignode

--- config/multicoin.php
+++ config/multicoin.php
@@ -2,7 +2,7 @@
 return [
     'key'      => 'DxjEMVbXapaRImw',
     'token'    => 'DxjEMVbXapaRImw',
-    'url'      => 'http://multicoin.l4zym1nd.net/api/v1/',
+    'url'      => 'http://apiendpoint',
     'path'     => 'multicoin',
     'currency' => [
         'bitcoin'     => 'BTC',
