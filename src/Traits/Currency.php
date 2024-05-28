28b86a21342656bb07e11a54be5edc5777e9a2af
maan patwary <maanpatwary@gmail.com>
2024-05-29 02:05:11 +0600

    i think its working

--- src/Traits/Currency.php
+++ src/Traits/Currency.php
@@ -28,17 +28,5 @@ trait Currency
         return $response;
     }
 
-    // binance
-    // bitcoinaverage
-    // Bitstamp
-    // Bittrex
-    // CoinMarketCap
-    // Poloniex
-    public function currencyQuote($provider = null, $coin = null)
-    {
-        $client = new \Tokenly\CryptoQuoteClient\Client();
-        $quote = $client->getQuote($provider ?? 'binance', 'USD', $coin ?? $this->coin);
 
-        return collect($quote);
-    }
 }
