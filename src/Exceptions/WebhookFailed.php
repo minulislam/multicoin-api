f39511f32f7a676172907e77a2ba514b40d22999
FooBar <FooBar@FooBar.bar>
2025-10-13 16:09:26 +0600

    fixes

--- src/Exceptions/WebhookFailed.php
+++ src/Exceptions/WebhookFailed.php
@@ -21,35 +21,40 @@ class WebhookFailed extends Exception
         );
     }
 
-    // ... existing code ...
     public static function invalidSignature(string $signature): self
     {
-        return new static("The signature `{$signature}` found in the header named `".self::SIGNATURE_HEADER.'` is invalid.
-            Make sure that the `'.self::API_KEY_CONFIG.'` config key is set to the value you found on the multicoin account.');
+        return new static("The signature `{$signature}` found in the header named `".self::SIGNATURE_HEADER.'` is invalid. '.
+            'Make sure that the `'.self::API_KEY_CONFIG.'` config key is set to the value you found on the multicoin account.');
     }
 
-    // ... existing code ...
     public static function missingSignature(): self
     {
         return new static('The request did not contain a header named `'.self::SIGNATURE_HEADER.'`.');
     }
 
-    // ... existing code ...
-    public static function missingType(Request $_request): self
+    public static function missingType(Request $request): self
     {
         return new static('The webhook call did not contain a type. Valid calls should always contain a type.');
     }
 
-    // ... existing code ...
-    public function render(Request $request)
+    public static function invalidPayload(Request $request): self
     {
-        return response()->json(['error' => $this->getMessage()], 400);
+        return new static('The webhook call did not contain any valid payload.');
     }
 
-    // ... existing code ...
+    public static function invalidJson(Request $request, string $error): self
+    {
+        return new static("The webhook call contained invalid JSON: {$error}");
+    }
+
     public static function signingSecretNotSet(): self
     {
-        return new static('The webhook signing secret is not set.
-            Make sure that the `'.self::API_KEY_CONFIG.'` config key is set.');
+        return new static('The webhook signing secret is not set. '.
+            'Make sure that the `'.self::API_KEY_CONFIG.'` config key is set.');
+    }
+
+    public function render($request)
+    {
+        return response()->json(['error' => $this->getMessage()], 400);
     }
 }
--- src/Http/Controllers/WebhookController.php
+++ src/Http/Controllers/WebhookController.php
@@ -2,8 +2,11 @@
 
 namespace Multicoin\Api\Http\Controllers;
 
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Bus;
+use Illuminate\Support\Facades\Log;
 use Multicoin\Api\Exceptions\WebhookFailed;
 use Multicoin\Api\Http\Middlewares\VerifySignature;
 use Multicoin\Api\WebhookCall;
@@ -15,33 +18,68 @@ class WebhookController extends Controller
         $this->middleware(VerifySignature::class);
     }
 
-    public function __invoke(Request $request)
+    public function __invoke(Request $request): JsonResponse
     {
         $eventPayload = $this->getJsonPayloadFromRequest($request);
+
         if (! isset($eventPayload['type'])) {
             throw WebhookFailed::missingType($request);
         }
+
         $type = $eventPayload['type'];
-        $WebhookCall = new WebhookCall($eventPayload);
-        event("multicoin-webhooks::{$type}", $WebhookCall);
+        $webhookCall = new WebhookCall($eventPayload);
+
+        // Log before processing
+        Log::info('Webhook received', [
+            'type' => $type,
+            'payload' => $eventPayload,
+        ]);
+
+        // Fire event
+        event("multicoin-webhooks::{$type}", $webhookCall);
+
         $jobClass = $this->determineJobClass($type);
+
         if ($jobClass === '') {
-            return response()->noContent();
-        }
-        if (! class_exists($jobClass)) {
-            throw WebhookFailed::jobClassDoesNotExist($jobClass, $WebhookCall);
+            Log::warning('No job configured for webhook type', ['type' => $type]);
+
+            return response()->json([
+                'status' => true,
+                'message' => 'Webhook received but no handler configured',
+            ]);
         }
 
-        return dispatch(new $jobClass($WebhookCall));
+        if (! class_exists($jobClass)) {
+            throw WebhookFailed::jobClassDoesNotExist($jobClass, $webhookCall);
+        }
+
+        Bus::dispatch(new $jobClass($webhookCall));
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Webhook processing initiated',
+        ]);
     }
 
-    protected function determineJobClass(string $type)
+    protected function determineJobClass(string $type): string
     {
         return config("multicoin.jobs.{$type}", '');
     }
 
-    private function getJsonPayloadFromRequest($request)
+    private function getJsonPayloadFromRequest(Request $request): array
     {
-        return (array) json_decode($request->getContent(), true);
+        $content = $request->getContent();
+
+        if (empty($content)) {
+            throw WebhookFailed::invalidPayload($request);
+        }
+
+        $payload = json_decode($content, true);
+
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            throw WebhookFailed::invalidJson($request, json_last_error_msg());
+        }
+
+        return $payload ?? [];
     }
 }
